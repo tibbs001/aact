06d58a262ed2480f6715792525625c6139677247
tibbs001@duke.edu <tibbs001@duke.edu>
2015-11-17 19:39:45 -0800

    Broke tests when I changed the database naming method (adding suffix to identify dev/prod)

--- app/models/aact/load_event.rb
+++ app/models/aact/load_event.rb
@@ -1,6 +1,6 @@
 module Aact
 	class LoadEvent < ActiveRecord::Base
 	  self.table_name='load_events'
-		establish_connection "aact_#{Rails.env}".to_sym
+		establish_connection "aact_#{Rails.env}".to_sym if Rails.env != 'test'
 	end
 end
